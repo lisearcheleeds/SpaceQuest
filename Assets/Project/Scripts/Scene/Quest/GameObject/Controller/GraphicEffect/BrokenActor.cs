5b3f48c6373cca9d9e8dbca097184262aa08ce79
LisetteLander <LisetteLander@gmail.com>
2023-06-17 19:15:44 +0900

    fix

--- Assets/Project/Scripts/Scene/Quest/GameObject/Controller/GraphicEffect/BrokenActor.cs
+++ Assets/Project/Scripts/Scene/Quest/GameObject/Controller/GraphicEffect/BrokenActor.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 namespace AloneSpace
 {
@@ -10,8 +11,11 @@ namespace AloneSpace
 
         float lifeTime;
         float currentLifeTime;
+        float smallTime;
 
         Vector3 movementVelocity;
+
+        Rigidbody[] pieces;
 
         protected override void OnInit()
         {
@@ -20,24 +24,41 @@ namespace AloneSpace
 
             movementVelocity = brokenActorGraphicEffectHandler.ActorData.MovingModule.MovementVelocity;
 
+            pieces = GetComponentsInChildren<Rigidbody>();
+
             transform.position = GraphicEffectHandler.PositionData.Position;
             transform.rotation = GraphicEffectHandler.PositionData.Rotation;
 
             currentLifeTime = 0.0f;
-            lifeTime = 1.0f;
+            lifeTime = 15.0f;
+            smallTime = 5.0f;
+
+            foreach (var piece in pieces)
+            {
+                var mesh = piece.GetComponent<MeshFilter>().mesh;
+                var centerOffset = mesh.bounds.center;
+                mesh.SetVertices(mesh.vertices.Select(v => v - centerOffset).ToArray());
+                mesh.RecalculateBounds();
+                piece.transform.localPosition = piece.transform.localPosition + centerOffset;
+
+                piece.AddForce(movementVelocity + Random.insideUnitSphere * 1.0f, ForceMode.Force);
+                piece.AddTorque(Random.insideUnitSphere * 1f, ForceMode.Force);
+            }
         }
 
         public override void OnLateUpdate(float deltaTime)
         {
             currentLifeTime += deltaTime;
+
+            foreach (var piece in pieces)
+            {
+                piece.transform.localScale = Vector3.one * (1.0f - Mathf.Clamp01((currentLifeTime - smallTime) / (lifeTime - smallTime)));
+            }
+
             if (currentLifeTime > lifeTime)
             {
                 IsCompleted = true;
-                return;
             }
-
-            transform.position += movementVelocity;
-            transform.localScale = Vector3.one * (1.0f - Mathf.Clamp01(currentLifeTime / lifeTime));
         }
     }
 }
