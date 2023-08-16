8896440e3d5a159352c7856aa9f389f6f670a60f
LisetteLander <LisetteLander@gmail.com>
2023-08-16 22:00:34 +0900

    fic

---
+++ Assets/Project/Scripts/Scene/Quest/Worker/SceneWorker/UI/MenuView/InventoryView/InventoryList.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using VariableInventorySystem;
+
+namespace AloneSpace
+{
+    public class InventoryList : MonoBehaviour
+    {
+        public StandardStashView StandardStashView => stashView;
+
+        [SerializeField] StandardStashView stashView;
+
+        QuestData questData;
+
+        public void Initialize(QuestData questData)
+        {
+            this.questData = questData;
+
+            var data = new VariableInventoryViewData(6, 6);
+            stashView.Apply(data);
+        }
+
+        public void Finalize()
+        {
+        }
+
+        public void OnUpdate()
+        {
+        }
+    }
+}
--- Assets/Project/Scripts/Scene/Quest/Worker/SceneWorker/UI/MenuView/InventoryView/InventoryView.cs
+++ Assets/Project/Scripts/Scene/Quest/Worker/SceneWorker/UI/MenuView/InventoryView/InventoryView.cs
@@ -1,20 +1,29 @@
-﻿using AloneSpace.Common;
-using UnityEngine;
+﻿using UnityEngine;
+using VariableInventorySystem;
 
 namespace AloneSpace
 {
     public class InventoryView : MonoBehaviour
     {
         [SerializeField] WeaponList weaponList;
+        [SerializeField] InventoryList inventoryList;
+
+        [SerializeField] StandardCore standardCore;
 
         public void Initialize(QuestData questData)
         {
+            standardCore.Initialize();
+            standardCore.AddInventoryView(inventoryList.StandardStashView);
+
             weaponList.Initialize(questData);
+            inventoryList.Initialize(questData);
+
         }
 
         public void Finalize()
         {
             weaponList.Finalize();
+            inventoryList.Finalize();
         }
 
         public void OnUpdate()
